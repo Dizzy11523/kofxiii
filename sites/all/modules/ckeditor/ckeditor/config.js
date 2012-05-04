7da99b9678de169a1a9ebd840db3cf63c4977be9
Alfredo <alfredo.nez@me.com>
2012-05-03 19:09:31 -0600

    first commit

---
+++ sites/all/modules/ckeditor/ckeditor/config.js
@@ -0,0 +1,15 @@
+﻿/*
+Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+};
+
+config.smiley_path=CKEDITOR.basePath+'plugins/smiley/images/kof/';
+config.smiley_images=['2guns.gif'];
+config.smiley_descriptions=['2gz'];
---
+++ sites/all/modules/smiley/modules/smiley_wysiwyg/plugins/smiley_extra/langs/en.js
@@ -0,0 +1,7 @@
+// $Id$
+
+tinyMCE.addToLang('smiley', {
+  title: 'Insert smileys',
+  desc: 'Adds a smiley button for WYSIWYG editors.'
+});
+
